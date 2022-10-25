297e2b4ba2e9554c2b3de18d3cf04762ef95cdb9
Vincent Stehlé <vincent.stehle@arm.com>
2022-10-25 17:06:43 +0200

    Fix EBBR4 M2
    Signed-off-by: Vincent Stehlé <vincent.stehle@arm.com>

--- download_everything.sh
+++ download_everything.sh
@@ -1,14 +1,15 @@
 #!/bin/sh
 
 git clone https://git.linaro.org/people/paul.liu/systemready/optee_os.git
-git -C optee_os checkout paulliu-compulab-rpmb
+git -C optee_os checkout --detach origin/paulliu-compulab-rpmb
+git -C optee_os am ../patches/0001-Fix-link-with-ld-2.39.patch
 
 git clone https://git.linaro.org/people/paul.liu/systemready/trusted-firmware-a.git
-git -C trusted-firmware-a checkout paulliu-imx8mm-emmc
+git -C trusted-firmware-a checkout bfc3de891
+git -C trusted-firmware-a am ../patches/0001-Fix-bl32-link-with-ld-2.39.patch
 
 git clone https://github.com/ARMmbed/mbedtls.git
-git -C mbedtls checkout development
-git -C mbedtls checkout 068a13d909ec08a12a5f74289b18142d27977044 -b test1
+git -C mbedtls checkout 068a13d909ec08a12a5f74289b18142d27977044
 
 if [ x"$USER" = x"paulliu" ]; then
     git clone --reference ~/upstream/u-boot ssh://git@git.linaro.org/people/paul.liu/systemready/u-boot.git
