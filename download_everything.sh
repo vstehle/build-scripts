c007b297ad39a3e5071340fa53d845da62a3ce3d
Vincent Stehlé <vincent.stehle@arm.com>
2022-10-25 17:06:43 +0200

    Fix EBBR3 M2
    Signed-off-by: Vincent Stehlé <vincent.stehle@arm.com>

--- download_everything.sh
+++ download_everything.sh
@@ -2,15 +2,15 @@
 
 # OPTEE
 git clone https://git.linaro.org/people/paul.liu/systemready/optee_os.git
-git -C optee_os checkout paulliu-compulab-rpmb
+git -C optee_os checkout 26fc000f
+git -C optee_os am ../patches/0001-Fix-link-with-ld-2.39.patch
 
 # TF-A
 git clone https://git.linaro.org/people/paul.liu/systemready/trusted-firmware-a.git
-git -C trusted-firmware-a checkout master
+git -C trusted-firmware-a checkout 24dc0a289
 
 git clone https://github.com/ARMmbed/mbedtls.git
-git -C mbedtls checkout development
-git -C mbedtls checkout f08ec01e2b4b3c91af8d822a1cdf17c8a5577035 -b test1
+git -C mbedtls checkout f08ec01e2b4b3c91af8d822a1cdf17c8a5577035
 
 # U-boot
 if [ x"$USER" = xpaulliu ]; then
